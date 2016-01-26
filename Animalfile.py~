c3cedc2a9f5c519f5b532907008d7cd3c910e23e
brit13-meet <brit13@meet.mit.edu>
2016-01-26 19:54:08 +0200

    oop

---
+++ Animalfile.py~
@@ -0,0 +1,13 @@
+class Animal(object):
+	def __init__(self, sound, name, age):
+		self.sound = sound
+		self.name = name
+		self.age = age
+	
+
+	def sleeping(self,sleep):
+		print(self.name +  " is sleeping " + sleep)
+	
+	def eat(self,food):
+		print(self.name + " is eating " + food)
+	
---
+++ uses.py
@@ -0,0 +1,14 @@
+from Animalfile import*
+#dog = Animal("woof","Jeff",17)
+#cat = Animal("mewo","Lolo",4)
+#print(dog.sound)
+import random
+#animals=["dog","cat","duck","bird","mouse","lion"}
+zoo = []
+for i in range(0,1000):
+	a = Animal("woof","Jeff",17)
+	b = Animal("mewo","Lolo",4)
+print(a.sound + str(a.age) + a.name)
+print(b.sound + str(b.age) + b.name)
+#dog.sleeping("happily")
+#cat.eat("Bisly")
